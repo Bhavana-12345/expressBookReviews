b8bb6dfc07340ca60ee839749791d1008565ed2c
Bhavana-12345 <90259496+Bhavana-12345@users.noreply.github.com>
2022-12-27 19:20:40 +0530

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -23,11 +23,6 @@ app.use("/customer/auth/*", function auth(req,res,next){
                return res.status(403).json({message: "User not authenticated"})
            }
         });
-    } else {
-        return res.status(403).json({message: "User not logged in"})
-    }
-});
- 
 const PORT =5000;
 
 app.use("/customer", customer_routes);
